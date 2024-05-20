947810a5f64f030acef14cf72f3c2e570358ec2b
RX-0 <zeal23310160@fsgsl.jp>
2024-05-20 12:15:19 +0900

    確認問題4.4

--- 初級Java/src/Rectlf.java
+++ 初級Java/src/Rectlf.java
@@ -5,9 +5,11 @@ public class Rectlf extends MyFrame{
 		setColor(0,128,0);
 		int i;
 		for(i=0;i<9;i++) {
-			if(i>=4 && i<=7) {
+			if(i==0) {
+				fillRect(x,60,10,100);
+			}else if(i==8) {
 				fillRect(x,100,10,100);
-			}else {
+		    }else {
 				fillRect(x,80,10,100);
 			}
 			x+=20;
