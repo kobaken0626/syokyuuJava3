0ebf89c0185c3db2a7669fe7e105f6cfca5c1eaa
RX-0 <zeal23310160@fsgsl.jp>
2024-05-20 11:42:37 +0900

    Java 4.3 for文を利用する

--- 初級Java/src/Start.java
+++ 初級Java/src/Start.java
@@ -4,7 +4,6 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("プログラムを実行します");
-        MyFrame frame1=new RectLoop(); 
+        MyFrame frame1=new RectLoop();
 	}
-
 }
