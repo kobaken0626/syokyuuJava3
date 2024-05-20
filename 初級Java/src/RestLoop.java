ffabd4fe98d1e7ac2f9a86ff1be9d26ca165052a
RX-0 <zeal23310160@fsgsl.jp>
2024-05-20 11:55:41 +0900

    Java 4.3 for文を利用する(修正版)

--- 初級Java/src/RestLoop.java
+++ 初級Java/src/RestLoop.java
@@ -4,7 +4,7 @@ public class RestLoop extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
 		int x=30;
-		while(x<=170) {
+		for(int i=0;i<15;i++) {
 			System.out.println(x);
 			fillRect(x,80,10,100);
 			x+=20;
