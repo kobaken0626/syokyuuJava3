3588db794d609573b66c33cdb619c0a089269194
RX-0 <zeal23310160@fsgsl.jp>
2024-05-20 10:06:13 +0900

    Java 3.6問題

--- 初級Java/src/RectTest.java
+++ 初級Java/src/RectTest.java
@@ -1,20 +1,10 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		setColor(255,255,0);
-		fillRect(30,70,60,70);
-		
-		setColor(205,0,255);
-		fillRect(130,70,60,70);
-		
-		setColor(0,255,255);
-		fillRect(30,200,150,10);
-		
-		setColor(160,160,155);
-		fillRect(90,160,40,30);
-		
-		setColor(0,0,0);
-		fillRect(90,120,40,10);
+		fillRect(40,100,20,20);
+		fillRect(30,110,40,10);
+		fillOval(35,120,10,10);
+		fillOval(55,120,10,10);
 	}
 
 }
