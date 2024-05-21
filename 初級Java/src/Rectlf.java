d275569279e576a8ae43cd957ddd840f0649f703
RX-0 <zeal23310160@fsgsl.jp>
2024-05-21 11:39:38 +0900

    追加課題その2

--- 初級Java/src/Rectlf.java
+++ 初級Java/src/Rectlf.java
@@ -1,14 +1,19 @@
 
 public class Rectlf extends MyFrame{
 	public void run() {
-		int x=20;
-		int y=15;
+		int x=30;
+		int y=160;
+		int z=40;
+		int a=300;
 		setColor(0,128,0);
 		int i;
 		for(i=0;i<9;i++) {
-		   fillRect(x,x,y,y);
-		   x+=y;
-		   y+=15;
+			fillRect(x,80,10,y);
+			fillRect(x,a,10,z);
+		   x+=20;
+		   y-=15;
+		   z+=15;
+		   a-=15;
 		}
 	}	
 }
