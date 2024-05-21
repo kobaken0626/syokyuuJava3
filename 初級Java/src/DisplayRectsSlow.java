f6afcf1457d18c4b7ea36bb7d36afe7a25078a9a
RX-0 <zeal23310160@fsgsl.jp>
2024-05-21 12:15:16 +0900

    Java 5.1 図形をゆっくり表示する(修正版)

--- 初級Java/src/DisplayRectsSlow.java
+++ 初級Java/src/DisplayRectsSlow.java
@@ -2,13 +2,10 @@
 public class DisplayRectsSlow extends MyFrame{
 	public void run() {
 		
+		setColor(0,128,0);
 		int x=30;
 		
 		while(x<=170) {
-			//(1)画面を消す
-			clear();
-			//(2)四角形を表示する
-			setColor(0,128,0);
 			fillRect(x,80,10,100);
 			x+=20;
 			sleep(1);
