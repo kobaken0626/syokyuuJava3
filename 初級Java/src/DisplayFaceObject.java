2b06113c23094013a73e8bf6325cf3d8bf0145b8
RX-0 <zeal23310160@fsgsl.jp>
2024-05-28 10:19:34 +0900

    Java 6.3 アニメーションできるようにする

--- 初級Java/src/DisplayFaceObject.java
+++ 初級Java/src/DisplayFaceObject.java
@@ -1,10 +1,21 @@
 
 public class DisplayFaceObject extends MyFrame{
 	public void run() {
-		Face face1=new Face(50,50);  //xとy
-		face1.draw(this);           //drawでの計算
-		
+		Face face1=new Face(50,50);
 		Face face2=new Face(200,100);
-		face2.draw(this);
+		
+		for(int i=0;i<30;i++) {
+			
+			clear();
+			
+			face1.draw(this);
+			face2.draw(this);
+			face1.x+=10;
+			face1.y+=5;
+			face2.x-=10;
+			face2.y-=5;
+			
+			sleep(0.1);
+		}
 	}
 }
