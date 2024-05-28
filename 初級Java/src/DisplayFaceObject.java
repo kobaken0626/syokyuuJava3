1aa8a16757d1d45c4ddd3da66a7caeba8add6d58
RX-0 <zeal23310160@fsgsl.jp>
2024-05-28 10:14:57 +0900

    Java 確認問題6.2

--- 初級Java/src/DisplayFaceObject.java
+++ 初級Java/src/DisplayFaceObject.java
@@ -4,7 +4,10 @@ public class DisplayFaceObject extends MyFrame{
 		Face face1=new Face(50,50);  //xとy
 		face1.draw(this);           //drawでの計算
 		
-		Face face2=new Face(200,100);
+		Face face2=new Face(250,50);
 		face2.draw(this);
+		
+		Face face3=new Face(150,150);
+		face3.draw(this);
 	}
 }
