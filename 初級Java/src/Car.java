f27f4f8ee60189c0707929aa16c64173b6a4f862
RX-0 <zeal23310160@fsgsl.jp>
2024-06-11 09:47:28 +0900

    Java 練習問題9-1

--- 初級Java/src/Car.java
+++ 初級Java/src/Car.java
@@ -24,11 +24,14 @@ public class Car extends Vehicle implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		
 			if(e.getKeyCode()==KeyEvent.VK_LEFT)
 			vx=-5;
 			if(e.getKeyCode()==KeyEvent.VK_RIGHT)
 			vx=5;
+			if(e.getKeyCode()==KeyEvent.VK_UP)
+			vy=-5;
+			if(e.getKeyCode()==KeyEvent.VK_DOWN)
+			vy=5;
 		
 	}
 
@@ -39,5 +42,9 @@ public class Car extends Vehicle implements KeyListener{
 		vx=0;
 		if(e.getKeyCode()==KeyEvent.VK_RIGHT)
 		vx=0;
+		if(e.getKeyCode()==KeyEvent.VK_UP)
+		vy=0;
+		if(e.getKeyCode()==KeyEvent.VK_DOWN)
+		vy=0;
 	}
 }
